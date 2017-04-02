8dbda9fbb3106060a1fef22f0daca4712f976dd9
Yusank <yusana@sina.cn>
2017-04-02 12:36:04 +0800

    修改代码规范 #20

--- common/timer/timer.go
+++ common/timer/timer.go
@@ -32,23 +32,23 @@ package timer
 
 import (
 	"time"
-    "sync"
-    "fmt"
+	"sync"
+	"fmt"
 
-    "hypercube/common/log"
+	"hypercube/common/log"
 )
 
 const (
-    timeFormat       = "2006-01-02 15:04:05"
-    infiniteDuration = time.Duration(1<<63 - 1)
+	timeFormat       = "2017-04-02 15:04:05"
+	infiniteDuration = time.Duration(1 << 63 - 1)
 )
 
 var common *log.S8ELogger = log.S8ECreateLogger(
-    &log.S8ELogTag{
-        log.LogTagService: "common",
-        log.LogTagType: "timer",
-    },
-    log.S8ELogLevelDefault)
+	&log.S8ELogTag{
+		log.LogTagService: "common",
+		log.LogTagType: "timer",
+	},
+	log.S8ELogLevelDefault)
 
 type TimerFunc func()
 
@@ -58,26 +58,26 @@ type Block interface {
 }
 
 type BlockBasic struct {
-    Key         string
-    expire      time.Time
-    fn          TimerFunc
-    index       int
-    next        *BlockBasic
+	Key         string
+	expire      time.Time
+	fn          TimerFunc
+	index       int
+	next        *BlockBasic
 }
 
 
 func (bb *BlockBasic) Expired() bool {
-    sub := bb.expire.Sub(time.Now())
+	sub := bb.expire.Sub(time.Now())
 
-    if sub > 0 {
-        return false
-    }
+	if sub > 0 {
+		return false
+	}
 
-    return true
+	return true
 }
 
 func (bb *BlockBasic) ExpiredString() string {
-    return bb.expire.Format(timeFormat)
+	return bb.expire.Format(timeFormat)
 }
 
 type Timer interface {
@@ -88,244 +88,259 @@ type Timer interface {
 }
 
 type TimeBasic struct {
-    lock        sync.Mutex
-    free        *BlockBasic
-    timers      []*BlockBasic
-    signal      *time.Timer
-    capacity    int
+	lock        sync.Mutex
+	free        *BlockBasic
+	timers      []*BlockBasic
+	signal      *time.Timer
+	capacity    int
 }
 
 func NewTimer(capacity int) (tb *TimeBasic) {
-    tb = new(TimeBasic)
-    tb.init(capacity)
-    return tb
+	tb = new(TimeBasic)
+	tb.init(capacity)
+
+	return tb
 }
 
 func (tb *TimeBasic) Create(capacity int) {
-    tb.init(capacity)
+	tb.init(capacity)
 }
 
 // 初始化 timer
 func (tb *TimeBasic) init(capacity int){
-    tb.signal = time.NewTimer(infiniteDuration)
-    tb.timers = make([]*BlockBasic, 0, capacity)
-    tb.capacity = capacity
-    tb.grow()
-    go tb.start()
+	tb.signal = time.NewTimer(infiniteDuration)
+	tb.timers = make([]*BlockBasic, 0, capacity)
+	tb.capacity = capacity
+	tb.grow()
+
+	go tb.start()
 }
 
 func (tb *TimeBasic) grow() {
-    var (
-        i   int
-        bb  *BlockBasic
-        bbs = make([]BlockBasic, tb.capacity)
-    )
+	var (
+		i   int
+		bb  *BlockBasic
+		bbs = make([]BlockBasic, tb.capacity)
+	)
 
-    tb.free = &(bbs[0])
-    bb = tb.free
+	tb.free = &(bbs[0])
+	bb = tb.free
 
-    for i = 1; i < tb.capacity; i++ {
-        bb.next = &(bbs[i])
-        bb = bb.next
-    }
-    bb.next = nil
+	for i = 1; i < tb.capacity; i++ {
+		bb.next = &(bbs[i])
+		bb = bb.next
+	}
+	bb.next = nil
 
-    return
+	return
 }
 
 func (tb *TimeBasic) get() (bb *BlockBasic) {
-    bb = tb.free
-    if bb == nil {
-        tb.grow()
-        bb = tb.free
-    }
+	bb = tb.free
+	if bb == nil {
+		tb.grow()
+		bb = tb.free
+	}
 
-    tb.free = bb.next
-    return
+	tb.free = bb.next
+
+	return
 }
 
 func (tb *TimeBasic) put(bb *BlockBasic) {
-    bb.fn = nil
-    bb.next = tb.free
-    tb.free = bb
+	bb.fn = nil
+	bb.next = tb.free
+	tb.free = bb
 }
 
 func (tb *TimeBasic) Add(expire  time.Duration, fn TimerFunc) (bb *BlockBasic) {
-    tb.lock.Lock()
-    bb = tb.get()
-    bb.expire = time.Now().Add(expire)
-    bb.fn = fn
-    tb.add(bb)
-    tb.lock.Unlock()
-    return
+	tb.lock.Lock()
+
+	bb = tb.get()
+	bb.expire = time.Now().Add(expire)
+	bb.fn = fn
+	tb.add(bb)
+	
+	tb.lock.Unlock()
+
+	return
 }
 
 func (tb *TimeBasic) Remove(bb *BlockBasic) {
-    tb.lock.Lock()
-    tb.remove(bb)
-    tb.put(bb)
-    tb.lock.Unlock()
-    return
+	tb.lock.Lock()
+
+	tb.remove(bb)
+	tb.put(bb)
+
+	tb.lock.Unlock()
+
+	return
 }
 
 // 更新 timer 数据
 func (tb *TimeBasic) Set(bb *BlockBasic, expire time.Duration) {
-    tb.lock.Lock()
-    tb.remove(bb)
-    bb.expire = time.Now().Add(expire)
-    tb.add(bb)
-    tb.lock.Unlock()
-    return
+	tb.lock.Lock()
+
+	tb.remove(bb)
+	bb.expire = time.Now().Add(expire)
+	tb.add(bb)
+
+	tb.lock.Unlock()
+
+	return
 }
 
 func (tb *TimeBasic) add(bb *BlockBasic) {
-    var d time.Duration
+	var d time.Duration
 
-    bb.index = len(tb.timers)
-    tb.timers = append(tb.timers, bb)
-    tb.up(bb.index)
+	bb.index = len(tb.timers)
+	tb.timers = append(tb.timers, bb)
+	tb.up(bb.index)
 
-    if bb.index == 0 {
-        d = bb.expire.Sub(time.Now())
-        tb.signal.Reset(d)
-        if false {
-            format := fmt.Sprintf("timer: add reset delay %d ms" , int64(d) / int64(time.Millisecond))
-            common.Debug(format)
-        }
-    }
-    if false {
-        format := fmt.Sprintf("timer: push item key: %s, expire: %s, index: %d", bb.Key, bb.ExpiredString(), bb.index)
-        common.Debug(format)
-    }
+	if bb.index == 0 {
+		d = bb.expire.Sub(time.Now())
+		tb.signal.Reset(d)
+
+		if false {
+			format := fmt.Sprintf("timer: add reset delay %d ms" , int64(d) / int64(time.Millisecond))
+			common.Debug(format)
+		}
+	}
+
+	if false {
+		format := fmt.Sprintf("timer: push item key: %s, expire: %s, index: %d", bb.Key, bb.ExpiredString(), bb.index)
+		common.Debug(format)
+	}
 
 }
 
 func (tb *TimeBasic) remove(bb *BlockBasic) {
-    var (
-        i     = bb.index
-        last  = len(tb.timers) - 1
-    )
+	var (
+		i     = bb.index
+		last  = len(tb.timers) - 1
+	)
 
-    if i < 0 || i > last || tb.timers[i] != bb {
-        // 已经被移除，通常是因为到期 (expire)
-        if false {
-            format := fmt.Sprintf("timer remove i: %d, last: %d, %p", i, last, bb)
-            common.Debug(format)
-        }
+	if i < 0 || i > last || tb.timers[i] != bb {
+		// 已经被移除，通常是因为到期 (expire)
+		if false {
+			format := fmt.Sprintf("timer remove i: %d, last: %d, %p", i, last, bb)
+			common.Debug(format)
+		}
 
-        return
-    }
+		return
+	}
 
-    if i != last {
-        tb.swap(i, last)
-        tb.down(i, last)
-        tb.up(i)
-    }
-    // 被移除的元素是最后一个节点的元素
-    tb.timers[last].index = -1
-    tb.timers = tb.timers[:last]
-    if false {
-        format := fmt.Sprintf("timer: remove item key: %s, expire: %s, index: %d", bb.Key, bb.ExpiredString(), bb.index)
-        common.Debug(format)
-    }
-    return
+	if i != last {
+		tb.swap(i, last)
+		tb.down(i, last)
+		tb.up(i)
+	}
+	// 被移除的元素是最后一个节点的元素
+	tb.timers[last].index = -1
+	tb.timers = tb.timers[:last]
+	if false {
+		format := fmt.Sprintf("timer: remove item key: %s, expire: %s, index: %d", bb.Key, bb.ExpiredString(), bb.index)
+		common.Debug(format)
+	}
+
+	return
 }
 
 func (tb *TimeBasic) start() {
-    for {
-        tb.expire()
-        <-tb.signal.C
-    }
+	for {
+		tb.expire()
+		<-tb.signal.C
+	}
 }
 
 func (tb *TimeBasic) expire() {
-    var (
-        fn TimerFunc
-        bb *BlockBasic
-        d  time.Duration
-    )
+	var (
+		fn TimerFunc
+		bb *BlockBasic
+		d  time.Duration
+	)
 
-    tb.lock.Lock()
+	tb.lock.Lock()
 
-    for {
-       if len(tb.timers) == 0 {
-           d = infiniteDuration
-           if false {
-               common.Debug("timer: no other instance")
-           }
-           break
-       }
+	for {
+	   if len(tb.timers) == 0 {
+		   d = infiniteDuration
+		   if false {
+			   common.Debug("timer: no other instance")
+		   }
+		   break
+	   }
 
-        bb = tb.timers[0]
+		bb = tb.timers[0]
 
-        if !bb.Expired() {
-            break
-        }
-        fn = bb.fn
-        tb.remove(bb)
-        tb.lock.Unlock()
+		if !bb.Expired() {
+			break
+		}
+		fn = bb.fn
+		tb.remove(bb)
+		tb.lock.Unlock()
 
-        if fn == nil {
+		if fn == nil {
 
-            common.Warn("expire timer no fn")
-        } else {
-            if false {
-                format := fmt.Sprintf("timer key: %s, expire: %s, index: %d expired, call fn", bb.Key, bb.ExpiredString(), bb.index)
-                common.Debug(format)
-            }
-            fn()
-        }
-        tb.lock.Lock()
-    }
-    tb.signal.Reset(d)
+			common.Warn("expire timer no fn")
+		} else {
+			if false {
+				format := fmt.Sprintf("timer key: %s, expire: %s, index: %d expired, call fn", bb.Key, bb.ExpiredString(), bb.index)
+				common.Debug(format)
+			}
+			fn()
+		}
+		tb.lock.Lock()
+	}
+	tb.signal.Reset(d)
 
-    if false {
-        format := fmt.Sprintf("timer: expire reset delay %d ms", int64(d) / int64(time.Millisecond))
-        common.Debug(format)
-    }
+	if false {
+		format := fmt.Sprintf("timer: expire reset delay %d ms", int64(d) / int64(time.Millisecond))
+		common.Debug(format)
+	}
 
-    tb.lock.Unlock()
+	tb.lock.Unlock()
 
-    return
+	return
 }
 
 func (tb *TimeBasic) up(j int) {
-    for {
-        i := (j - 1) / 2
+	for {
+		i := (j - 1) / 2
 
-        if j <= 0 || !tb.less(j, i) {
-            break
-        }
-        tb.swap(i, j)
-        j = i
-    }
+		if j <= 0 || !tb.less(j, i) {
+			break
+		}
+		tb.swap(i, j)
+		j = i
+	}
 }
 
 func (tb *TimeBasic) down(i, n int) {
-            for {
-                jl := 2 * i + 1
-                if jl >= n || jl < 0 {
-            break
-        }
-        j := jl
-        jr := jl + 1
-        if jr < n && !tb.less(jl, jr) {
-            j = jr  // =2*i + 2
-        }
-        if !tb.less(j, i) {
-            break
-        }
-        tb.swap(i, j)
-        i = j
-    }
+			for {
+				jl := 2 * i + 1
+				if jl >= n || jl < 0 {
+			break
+		}
+		j := jl
+		jr := jl + 1
+		if jr < n && !tb.less(jl, jr) {
+			j = jr  // =2*i + 2
+		}
+		if !tb.less(j, i) {
+			break
+		}
+		tb.swap(i, j)
+		i = j
+	}
 }
 
 func (tb *TimeBasic) less(i, j int) bool {
-    return tb.timers[i].expire.Before(tb.timers[j].expire)
+	return tb.timers[i].expire.Before(tb.timers[j].expire)
 }
 
 func (tb *TimeBasic) swap(i, j int) {
-    tb.timers[i], tb.timers[j] = tb.timers[j], tb.timers[i]
-    tb.timers[i].index = i
-    tb.timers[j].index = j
+	tb.timers[i], tb.timers[j] = tb.timers[j], tb.timers[i]
+	tb.timers[i].index = i
+	tb.timers[j].index = j
 }
