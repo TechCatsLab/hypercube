b0616123e33977f2f71a40df5d0c2bc82394fa67
Jsharkc <1773867204@qq.com>
2017-04-02 23:29:29 +0800

    单对单协议 / 通用 Proto #19

---
+++ proto/access/proto.go
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/02        Liu Jiachang
+ */
+
+package access
+
+import "fmt"
+
+var (
+	emptyProto = Proto{}
+	emptyJSONBody = []byte("{}")
+)
+
+type Proto struct {
+	Ver     int16       `json:"ver"`
+	Type    int32       `json:"type"`
+	SeqId   int32       `json:"seqid"`
+	Body    string      `json:"body"`
+}
+
+func (p *Proto) Reset() {
+	*p = emptyProto
+}
+
+func (p *Proto) String() string {
+	return fmt.Sprintf("\n-------- proto --------\nver: %d\ntype: %d\nseq: %d\nbody: %v\n--------------------", p.Ver, p.Type, p.SeqId, p.Body)
+}
