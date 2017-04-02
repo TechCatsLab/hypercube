ba848613c41d0cc2f84bfd33a711a8aac8923d8d
Yusank <yusana@sina.cn>
2017-04-02 12:38:05 +0800

    删除 panic 添加 error #30
    把 panic 换成 error，并修改代码规范

--- common/timer/timingwheel.go
+++ common/timer/timingwheel.go
@@ -30,83 +30,89 @@
 package timer
 
 import (
-    "sync"
-    "time"
+	"sync"
+	"time"
+	"errors"
+)
+
+var (
+	ErrTimingTimeout = errors.New("timeout toot much, over maxtimeout")
 )
 
 type TimingWheel struct {
-    sync.Mutex
-    interval    time.Duration
-    ticker      *time.Ticker
-    quit        chan struct{}
-    maxTimeout  time.Duration
-    cs          []chan struct{}
-    pos         int
+	sync.Mutex
+	interval    time.Duration
+	ticker      *time.Ticker
+	quit        chan struct{}
+	maxTimeout  time.Duration
+	cs          []chan struct{}
+	pos         int
 }
 
 func NewTimingWheel(interval time.Duration, buckets int) *TimingWheel {
-    w := new(TimingWheel)
+	w := new(TimingWheel)
 
-    w.interval = interval
-    w.quit = make(chan struct{})
-    w.pos = 0
-    w.maxTimeout = time.Duration(interval * (time.Duration(buckets)))
-    w.cs = make([]chan struct{}, buckets)
+	w.interval = interval
+	w.quit = make(chan struct{})
+	w.pos = 0
+	w.maxTimeout = time.Duration(interval * (time.Duration(buckets)))
+	w.cs = make([]chan struct{}, buckets)
 
-    for i := range w.cs {
-        w.cs[i] = make(chan struct{})
-    }
+	for i := range w.cs {
+		w.cs[i] = make(chan struct{})
+	}
 
-    w.ticker = time.NewTicker(interval)
-    go w.run()
+	w.ticker = time.NewTicker(interval)
 
-    return w
+	go w.run()
+
+	return w
 }
 
 func (w *TimingWheel) Stop() {
-    close(w.quit)
+	close(w.quit)
 }
 
-func (w *TimingWheel) After(timeout time.Duration) <-chan struct{} {
-    if timeout >= w.maxTimeout {
-        panic("timeout too much, over maxtimeout")
-    }
+func (w *TimingWheel) After(timeout time.Duration) (<-chan struct{}, error) {
+	if timeout >= w.maxTimeout {
+		return nil, ErrTimingTimeout
+	}
 
-    index := int(timeout / w.interval)
-    if index > 0 {
-        index--
-    }
+	index := int(timeout / w.interval)
+	if index > 0 {
+		index--
+	}
 
-    w.Lock()
+	w.Lock()
 
-    index = (w.pos + index) % len(w.cs)
-    b := w.cs[index]
+	index = (w.pos + index) % len(w.cs)
+	b := w.cs[index]
 
-    w.Unlock()
+	w.Unlock()
 
-    return b
+	return b, nil
 }
 
 func (w *TimingWheel) run() {
-    for {
-        select {
-        case <-w.ticker.C:
-            w.onTicker()
-        case <-w.quit:
-            w.ticker.Stop()
-            return
-        }
-    }
+	for {
+		select {
+		case <-w.ticker.C:
+			w.onTicker()
+		case <-w.quit:
+			w.ticker.Stop()
+			return
+		}
+	}
 }
 
 func (w *TimingWheel) onTicker() {
-    w.Lock()
+	w.Lock()
 
-    lastC := w.cs[w.pos]
-    w.cs[w.pos] = make(chan struct{})
-    w.pos = (w.pos + 1) % len(w.cs)
+	lastC := w.cs[w.pos]
+	w.cs[w.pos] = make(chan struct{})
+	w.pos = (w.pos + 1) % len(w.cs)
 
-    w.Unlock()
+	w.Unlock()
 
-    close(lastC)
+	close(lastC)
 }
