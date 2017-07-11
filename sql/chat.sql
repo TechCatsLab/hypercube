76a166d392210a781b45cddd0e7f7d65b1f7f465
Yusank <yusankurban@gmail.com>
2017-07-11 19:32:30 +0800

    修改 SQL 文件字段类型#106

--- sql/chat.sql
+++ sql/chat.sql
@@ -2,11 +2,11 @@ CREATE DATABASE IF NOT EXISTS core;
 SET DATABASE = core;
 
 CREATE TABLE IF NOT EXISTS message (
-  messageid     SERIAL        PRIMARY KEY,
-  source          INT,
-  target          INT         NOT NULL,
-  type            INT         NOT NULL,
-  issend          BOOL        DEFAULT FALSE,
+  messageid       SERIAL            PRIMARY KEY,
+  source          STRING(128),
+  target          STRING(128)     NOT NULL,
+  type            INT             NOT NULL,
+  issend          BOOL            DEFAULT FALSE,
   content         STRING(256),
-  created         TIMESTAMP   DEFAULT current_timestamp()
+  created         TIMESTAMP       DEFAULT current_timestamp()
 );
